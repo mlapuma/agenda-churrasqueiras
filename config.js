d08549d4314a647ae995c8a7596100437c017d43
marcio.puma <marcio.puma@saude.gov.bt>
2026-05-07 16:34:34 -0300

    Configura Supabase para reservas compartilhadas

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://jdjvkdjidttitupxoibi.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable__C_VuLYtMuneK3HAgau2Sw_aYK9doll"
 };
